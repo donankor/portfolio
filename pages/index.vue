3caa2c5db5a4fa3023ac69679738f514eeb472e8
donankor <a.koroliov98@gmail.com>
2023-02-05 14:54:47 +0300

    portfolio

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="cont">
     <Skills />
-    <Contacts />
   </div>
 </template>
 
 <script setup>
 import Skills from "~/components/Features/Skills.vue";
-import Contacts from "~/components/Features/Contacts.vue";
 </script>
 
 <style lang="scss" scoped>
