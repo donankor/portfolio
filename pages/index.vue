84ac46ca4f8ba3fdca35f6893de456b89de41258
donankor <a.koroliov98@gmail.com>
2023-02-05 14:43:46 +0300

    portfolio

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="cont">
-<!--    <Contacts/>-->
-<!--    <Education/>-->
-<!--    <Skills/>-->
-<!--    <Experience/>-->
+    <Contacts/>
   </div>
 </template>
 
 <script setup>
-import Contacts from "~/components/Features/Сontacts.vue";
-import Education from "~/components/Features/Education.vue";
-import Skills from "~/components/Features/Skills.vue";
-import Experience from "~/components/Features/Experience.vue";
+  import Contacts from "~/components/Features/Сontacts.vue";
 </script>
 
 <style lang="scss" scoped>
